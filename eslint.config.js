f9f52631382ef7caf8853809b909f62bda92fb2a
Azamat Omirtay <omirtayazamatt@gmail.com>
2026-06-22 10:50:21 +0500

    chore: migrate project setup from vite to next.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import reactPlugin from "eslint-plugin-react";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactPlugin from 'eslint-plugin-react';
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist", "coverage"]),
+  globalIgnores(['dist', 'coverage', '.next', 'next-env.d.ts']),
   {
-    files: ["**/*.{test,spec}.{ts,tsx}"],
+    files: ['**/*.{test,spec}.{ts,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -19,14 +18,13 @@ export default defineConfig([
     },
   },
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
       reactPlugin.configs.flat.recommended,
-      reactPlugin.configs.flat["jsx-runtime"],
+      reactPlugin.configs.flat['jsx-runtime'],
       eslintConfigPrettier,
     ],
     languageOptions: {
